aa279841ca5104971608c3b5e45f77c7c77a6d20
João Matheus Gardim <53411249+jmgardim@users.noreply.github.com>
2022-01-09 15:35:48 -0300

    Delete Front-end/JavaScript/lesson44_functions_with_multiple_parameters directory

--- Front-end/JavaScript/lesson44_functions_with_multiple_parameters/index.js
+++
@@ -1,8 +0,0 @@
-const welcomeEl = document.getElementById("welcome-el")
-
-// Give the function a parameter, greeting, that replaces "Welcome back"
-function greetUser(greeting , name, emoji) {
-    welcomeEl.textContent =  `${greeting}, ${name}  ${emoji}`  
-}
-
-greetUser("Howdy","per", "👋")
